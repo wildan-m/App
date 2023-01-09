4a1775aa74a4782f85a39dcddb8bbe6bfc9a2870
Wildan Muhlis <muhliswildan@gmail.com>
2023-01-10 00:02:30 +0700

    Fix Incorrect text styling

--- src/components/HTMLEngineProvider/HTMLRenderers/TextRenderer.js
+++ src/components/HTMLEngineProvider/HTMLRenderers/TextRenderer.js
@@ -1,21 +1,18 @@
 import _ from 'underscore';
 import React from 'react';
+import {View} from 'react-native';
 import htmlRendererPropTypes from './htmlRendererPropTypes';
 import Text from '../../Text';
-import  { getNativePropsForTNode, splitBoxModelStyle } from 'react-native-render-html';
+import  { getNativePropsForTNode, splitBoxModelStyle, TNodeChildrenRenderer } from 'react-native-render-html';
 
 const TextRenderer = (props) => {
     const nativeProps = getNativePropsForTNode(props);
-
-    // We split wrapper and inner styles
-    // "boxModelStyle" corresponds to border, margin, padding and backgroundColor
-    const {boxModelStyle, otherStyle: textStyle} = splitBoxModelStyle(props.style);
-
-
+    const textStyle = props.tnode.styles.nativeTextFlow;
+    
     const defaultRendererProps = _.omit(nativeProps, ['style']);
     // This is equivalent to a TDefaultRenderer which `Text` is replaced
     // with custom Expensify `Text` component 
-    return <Text style={{...boxModelStyle, ...textStyle}} {...defaultRendererProps}/>;
+    return <Text style={textStyle} {...defaultRendererProps}/>;
 };
 
 TextRenderer.propTypes = htmlRendererPropTypes;
