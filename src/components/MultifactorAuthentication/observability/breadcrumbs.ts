c099c399522ee01e5bf63382978f126db86c555a
Dariusz Biela <dariusz.biela.dev@gmail.com>
2026-04-20 17:36:32 +0200

    refactor: remove unused MfaError type import from breadcrumbs

--- src/components/MultifactorAuthentication/observability/breadcrumbs.ts
+++ src/components/MultifactorAuthentication/observability/breadcrumbs.ts
@@ -1,6 +1,5 @@
 import type {SeverityLevel} from '@sentry/react-native';
 import * as Sentry from '@sentry/react-native';
-import type {MfaError} from '@libs/MultifactorAuthentication/shared/MfaResult';
 import CONST from '@src/CONST';
 
 type BreadcrumbData = Record<string, string | number | boolean | undefined>;
