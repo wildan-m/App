ee1ccd13b971681a91327b7a32b87321cbb8534b
Nabi Ebrahimi <mnabijan049@gmail.com>
2026-04-21 15:08:56 +0430

    fix(expense): resolve no action when clicking "Pay with personal account" in QAB

--- src/libs/Navigation/types.ts
+++ src/libs/Navigation/types.ts
@@ -1995,6 +1995,12 @@ type MoneyRequestNavigatorParamList = {
         backToReport?: string;
         reportActionID?: string;
     };
+    [SCREENS.MONEY_REQUEST.CREATE_VERIFY_ACCOUNT]: {
+        action: IOUAction;
+        iouType: IOUType;
+        transactionID: string;
+        reportID: string;
+    };
     [SCREENS.MONEY_REQUEST.STEP_DISTANCE_RATE]: {
         action: IOUAction;
         iouType: ValueOf<typeof CONST.IOU.TYPE>;
