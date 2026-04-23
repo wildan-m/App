3ecaaf7b6a848be993aae01ccad78fa8b313db66
Michał Muzyk <mimuzyk@gmail.com>
2026-04-23 16:31:03 +0200

    fix: cr

--- src/libs/continuePlaidOAuth/index.ts
+++ src/libs/continuePlaidOAuth/index.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- Parameter kept for API parity with the iOS implementation (index.ios.ts)
 function continuePlaidOAuth(_url: string): void {
     // Plaid OAuth return is handled only on iOS HybridApp (see issue #87757).
 }
