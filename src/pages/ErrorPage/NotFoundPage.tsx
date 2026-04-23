c5102eea25a611aca0556894b16611a447b4f33a
Bernhard Owen Josephus (via MelvinBot) <infra+melvinbot@expensify.com>
2026-04-23 03:39:25 +0000

    Revert NotFoundPage changes per reviewer feedback
    Co-authored-by: Bernhard Owen Josephus <bernhardoj@users.noreply.github.com>

--- src/pages/ErrorPage/NotFoundPage.tsx
+++ src/pages/ErrorPage/NotFoundPage.tsx
@@ -1,3 +1,4 @@
+import {useRoute} from '@react-navigation/native';
 import React from 'react';
 import type {FullPageNotFoundViewProps} from '@components/BlockingViews/FullPageNotFoundView';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
@@ -7,6 +8,8 @@ import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import Navigation from '@libs/Navigation/Navigation';
 import useAbsentPageSpan from '@libs/telemetry/useAbsentPageSpan';
 import ONYXKEYS from '@src/ONYXKEYS';
+import ROUTES from '@src/ROUTES';
+import SCREENS from '@src/SCREENS';
 
 type NotFoundPageProps = {
     onBackButtonPress?: () => void;
@@ -21,6 +24,9 @@ function NotFoundPage({onBackButtonPress = () => Navigation.goBack(), isReportRe
     const {isSmallScreenWidth} = useResponsiveLayout();
     const topmostReportId = Navigation.getTopmostReportId();
     const [report] = useOnyx(`${ONYXKEYS.COLLECTION.REPORT}${topmostReportId}`);
+    const route = useRoute();
+    const isOnGenericErrorScreen = route.name === SCREENS.NOT_FOUND;
+
     useAbsentPageSpan();
 
     return (
@@ -45,7 +51,7 @@ function NotFoundPage({onBackButtonPress = () => Navigation.goBack(), isReportRe
                 }}
                 // eslint-disable-next-line react/jsx-props-no-spreading
                 {...fullPageNotFoundViewProps}
-                onLinkPress={fullPageNotFoundViewProps.onLinkPress}
+                onLinkPress={isOnGenericErrorScreen ? () => Navigation.goBack(ROUTES.HOME) : fullPageNotFoundViewProps.onLinkPress}
             />
         </ScreenWrapper>
     );
