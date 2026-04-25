46ce3133c1adf7ea7c84a56a610d68ddeae8eac5
Rajat Parashar <parasharrajat@users.noreply.github.com>
2026-04-25 15:02:21 +0530

    Update put on Hold function to use current user data from params

--- tests/actions/IOUTest.ts
+++ tests/actions/IOUTest.ts
@@ -6871,7 +6871,7 @@ describe('actions/IOU', () => {
 
                 // Put the expense on hold
                 if (originalTransactionID && transactionThreadReportID) {
-                    putOnHold(originalTransactionID, 'Test hold reason', transactionThreadReportID, false);
+                    putOnHold(originalTransactionID, 'Test hold reason', transactionThreadReportID, false, RORY_EMAIL, RORY_ACCOUNT_ID);
                 }
                 await waitForBatchedUpdates();
 
