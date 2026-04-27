e20fd89c65d6861bf4de4d82ea79f9b542a256ad
madmax330 <maxence@expensify.com>
2026-04-27 22:38:49 +0000

    remove comment

--- src/libs/API/parameters/OpenPolicyExpensifyCardsPageParams.ts
+++ src/libs/API/parameters/OpenPolicyExpensifyCardsPageParams.ts
@@ -1,6 +1,5 @@
 type OpenPolicyExpensifyCardsPageParams = {
     policyID: string;
-    /** Domain or workspace account ID context for Expensify Card */
     domainAccountID?: number;
     emailList?: string;
 };
